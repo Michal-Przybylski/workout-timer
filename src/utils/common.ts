a08e4866fb246ebfd829c0ac2b27d7bc8fde061b
Michal-Przybylski <michal.przybylski3@gmail.com>
2020-09-16 15:34:51 +0200

    add util fn to parse ms into hours & minutes & seconds

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,2 +1,10 @@
 export const tallyMiliseconds = (minutes = 0, seconds: number) =>
   (minutes * 60 + seconds) * 1000;
+
+export const msToHMS = (ms: number) => {
+  const totalSeconds = ms / 1000;
+  const seconds = Math.floor(totalSeconds % 60);
+  const minutes = Math.floor((totalSeconds / 60) % 60);
+  const hours = Math.floor((totalSeconds / 3600) % 24);
+  return { seconds, minutes, hours };
+};
