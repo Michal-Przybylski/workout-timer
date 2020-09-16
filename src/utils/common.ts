c40a9e76e0263793de2ff5e230e7103bf4c85956
Michal-Przybylski <michal.przybylski3@gmail.com>
2020-09-16 20:42:27 +0200

    remove unnecessary util fn

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,6 +1,3 @@
-export const tallyMiliseconds = (minutes = 0, seconds: number) =>
-  (minutes * 60 + seconds) * 1000;
-
 export const msToHMS = (ms: number) => {
   const totalSeconds = ms / 1000;
   const seconds = Math.floor(totalSeconds % 60);
